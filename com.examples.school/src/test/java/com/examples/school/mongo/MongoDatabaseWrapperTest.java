63bd121030f037ebfb5b7a8c6fad7047d17f785f
Matteo Ghera <matteo.ghera@stud.unifi.it>
2016-12-05 11:58:08 +0100

    added MongoTestHelper class for refactoring

--- com.examples.school/src/test/java/com/examples/school/mongo/MongoDatabaseWrapperTest.java
+++ com.examples.school/src/test/java/com/examples/school/mongo/MongoDatabaseWrapperTest.java
@@ -8,26 +8,21 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.examples.school.Student;
+import com.examples.school.mongo.helper.MongoTestHelper;
 import com.github.fakemongo.Fongo;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 
 public class MongoDatabaseWrapperTest {
 	private MongoDatabaseWrapper mongoDatabase;
-	private DBCollection students;
+	private MongoTestHelper mongoTestHelper;
 
 	@Before
 	public void initDB() throws UnknownHostException {
 		Fongo fongo = new Fongo("mongo server 1");
 		MongoClient mongoClient = fongo.getMongo();
-
-		DB db = mongoClient.getDB("school");
-		db.getCollection("student").drop();
-
+		mongoTestHelper=new MongoTestHelper(mongoClient);
+		
 		mongoDatabase = new MongoDatabaseWrapper(mongoClient);
-		students = db.getCollection("student");
 	}
 
 	@Test
@@ -37,33 +32,26 @@ public class MongoDatabaseWrapperTest {
 
 	@Test
 	public void testGetAllStudentsNotEmpty() {
-		addStudent("1", "first");
-		addStudent("2", "second");
+		mongoTestHelper.addStudent("1", "first");
+		mongoTestHelper.addStudent("2", "second");
 
 		assertEquals(2, mongoDatabase.getAllStudentsList().size());
 	}
 
 	@Test
 	public void testFindStudentByIdNotFound() {
-		addStudent("1", "first");
+		mongoTestHelper.addStudent("1", "first");
 		assertNull(mongoDatabase.findStudentById("2"));
 	}
 
 	@Test
 	public void testFindStudentByIdFound() {
-		addStudent("1", "first");
-		addStudent("2", "second");
+		mongoTestHelper.addStudent("1", "first");
+		mongoTestHelper.addStudent("2", "second");
 
 		Student findStudentById = mongoDatabase.findStudentById("2");
 		assertNotNull(findStudentById);
 		assertEquals("2", findStudentById.getId());
 		assertEquals("second", findStudentById.getName());
-	}
-
-	private void addStudent(String id, String name) {
-		BasicDBObject document = new BasicDBObject();
-		document.put("id", id);
-		document.put("name", name);
-		students.insert(document);
 	}
 }
